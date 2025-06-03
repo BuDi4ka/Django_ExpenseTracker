bcf17af6f87930ad8c1ed4e000d8d4cd2162e53b
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-03 13:48:45 +0100

    added view and url to handle user validation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path 
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('register/', views.RegistrationView.as_view(), name='register'),
+    path("register", views.RegistrationView.as_view(), name="register"),
+    path(
+        "validate-username",
+        csrf_exempt(views.UsernameValidationView.as_view()),
+        name="validate-username",
+    ),
 ]
-
